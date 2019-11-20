c28539b99a4ad53ceba9005a080f3382d63a1154
Jake Scott <bemybanker@gmail.com>
2019-11-20 15:52:02 -0600

    Fixtures complete, api accessible form port

--- ItsAlive/views/__init__.py
+++ ItsAlive/views/__init__.py
@@ -1,5 +1,7 @@
-from .product import Products
-from .customers import Customers
-from .orderproduct import OrderProducts
+from .orderproduct import OrderProducts, OrderProductSerializer
+from .product import Products, ProductSerializer
+from .register import register_user, login_user
 from .paymenttype import PaymentTypes
+from .customers import Customers
+from .user import UserViewSet
 from .orders import Orders
